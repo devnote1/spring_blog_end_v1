4ffa662cefdcd57f0e62d076c1b34785466a6b01
unknown <insertvalue@naver.com>
2024-03-13 15:46:42 +0900

    10. 게시글 상세보기

--- src/main/java/shop/tenco/blog/board/BoardController.java
+++ src/main/java/shop/tenco/blog/board/BoardController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -27,9 +30,17 @@ public class BoardController {
     public String saveForm() {
         return "board/saveForm";
     }
-
-    @GetMapping("/board/1")
-    public String detail() {
+    
+    
+    // Spring MVC에서 URI의 경로 변수를 메소드의 
+    // 매개변수로 바인딩하려면 @PathVariable 어노테이션을 사용해야 합니다.
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable("id") int id, HttpServletRequest request) {
+    	System.out.println("id : " + id);
+    		
+    	BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
+    	request.setAttribute("board", responseDTO);
+    	
         return "board/detail";
     }
 }
--- src/main/java/shop/tenco/blog/board/BoardRepository.java
+++ src/main/java/shop/tenco/blog/board/BoardRepository.java
@@ -19,4 +19,44 @@ public class BoardRepository {
         // List<Board> boardListEntity = query.getResultList();  
         return query.getResultList();
     }
+	
+	public BoardResponse.DetailDTO findById(int idx) {
+		// getSingleResult() 결과가 없으면 예외 클래스를 반환 합니다.  
+		BoardResponse.DetailDTO responseDTO = new BoardResponse.DetailDTO();
+		
+		try {
+			// 메서드명 확인 (createNativeQuery)
+			Query query = em.createNativeQuery("select b.id, b.title, b.content, b.user_id, u.username "
+					+ "from board_tb b "
+					+ "inner join user_tb u on b.user_id = u.id "
+					+ "where b.id = ?1 ");
+			query.setParameter(1, idx);
+			
+			// 컬럼이 여러개 -> Object[] 배열로 넘어 온다. 
+			Object[] row = (Object[]) query.getSingleResult();
+			Integer id = (Integer) row[0];
+	        String title = (String) row[1];
+	        String content = (String) row[2];
+	        int userId = (Integer) row[3];
+	        String username = (String) row[4];
+
+	        System.out.println("id : "+id);
+	        System.out.println("title : "+title);
+	        System.out.println("content : "+content);
+	        System.out.println("userId : "+userId);
+	        System.out.println("username : "+username);
+	        
+	        
+	        responseDTO.setId(id);
+	        responseDTO.setTitle(title);
+	        responseDTO.setContent(content);
+	        responseDTO.setUserId(userId);
+	        responseDTO.setUsername(username);
+	        
+		} catch (Exception e) {
+			System.err.println("findById 예외 발생 : " + e.getClass());
+			System.err.println("msg: " + e.getMessage());
+		}
+		return responseDTO; 
+	}
 }
